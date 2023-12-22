9270498ff9a838c5735a900828034ca729ba2e7c
Cat Meow <110690475+catMeooww@users.noreply.github.com>
2023-12-22 15:00:33 -0300

    Add files via upload

--- js/Question.js
+++ js/Question.js
@@ -3,33 +3,29 @@ class Question {
   constructor() {
     this.title = createElement('h1')
     this.input1 = createInput("Digite Seu Nome Aqui....");
-    this.input2 = createInput("Digite o Nº da Opção Correta...");
+    this.input2 = createInput("Digite o Nº da Opção Correta..");
     this.button = createButton('Enviar');
+    this.button2 = createButton('Reiniciar');
     this.question = createElement('h3');
     this.option1 = createElement('h4');
     this.option2 = createElement('h4');
     this.option3 = createElement('h4');
     this.option4 = createElement('h4');
-    this.message = createElement("h2")
   }
 
   hide(){
     this.title.hide();
     this.input1.hide();
     this.button.hide();
+    //this.button2.hide();
     this.input2.hide();
-    this.message.hide();
   }
 
   display(){
     this.title.html("Meu Questionário");
     this.title.position(350, 0);
 
-    // this.question("Pergunta:- O que é que está no meio do começo, no começo do meio, e no final do fim? " );
-    // this.html("Pergunta:- O que é que está no meio do começo, no começo do meio, e no final do fim? " );
-    this.question.html("Pergunta:- O que é que está no meio do começo, no começo do meio, e no final do fim? " );
-    // question.html("Pergunta:- O que é que está no meio do começo, no começo do meio, e no final do fim? " );
-
+    this.question.html("Pergunta: - O que é que está no meio do começo, no começo do meio, e no final do fim? " );
     this.question.position(150, 80);
     this.option1.html("1: A letra E " );
     this.option1.position(150, 100);
@@ -43,22 +39,26 @@ class Question {
     this.input1.position(150, 230);
     this.input2.position(350, 230);
     this.button.position(290, 300);
+    this.button2.position(360, 300);
 
     this.button.mousePressed(()=>{
-      
-       this.message.html("Obrigado, Sua Resposta Foi Enviada");
-       this.message.position(350, 350);
-
-      // this.message.html("Obrigado, Sua Resposta Foi Enviada");
-      // this.message.position();
-
-      // this.message("Obrigado, Sua Resposta Foi Enviada");
-      // this.message(350, 350);
-
-      // this.message.html("Obrigado, Sua Resposta Foi Enviada");
-      // this.message.position(350);
-
-
+      this.title.hide();
+      this.input1.hide();
+      this.input2.hide();
+      this.button.hide();
+      contestant.name = this.input1.value();
+      contestant.answer = this.input2.value();
+      contestantCount+=1;
+      contestant.index = contestantCount;
+      contestant.update();
+      contestant.updateCount(contestantCount);
     });
+
+    this.button2.mousePressed(()=>{
+      contestant.updateCount(0)
+      quiz.update(0)
+      var ref = database.ref("contestants")
+      ref.remove()
+    })
   }
 }
